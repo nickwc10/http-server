432ab60e5d8b978a76da41687a94825db3e18332
Nicholas Christensen <pixeltasim@gmail.com>
2026-03-04 17:27:39 -0500

    Added log for requests.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
