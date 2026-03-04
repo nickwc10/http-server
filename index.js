70232adf6fd1ab76fc4099c466d5d5f3590a924c
Nicholas Christensen <pixeltasim@gmail.com>
2026-03-04 17:21:05 -0500

    First commit.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code here
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(8080, () => {
+  console.log('Server running at http://localhost:8080/');
+});
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,33 +1,21 @@
-var Nightmare = require('nightmare');
 var expect = require('chai').expect;
 var axios = require('axios');
 
-var nightmare;
 const url = 'http://localhost:8080';
 
 describe('hello-express', function () {
   this.timeout(6500);
   this.slow(3000);
-  
-  beforeEach(()=> {
-    nightmare = Nightmare();
-  })
-  
+
   it('should have the correct page title', () =>
-    nightmare
-      .goto(url)
-      .evaluate(function () {
-        return document.querySelector('body').innerText
-      })
-      .end()
-      .then(function (text) {
-        expect(text).to.equal('Hello World');
-      })
-  );
-  
-  it('returns the correct status code', () => axios.get('http://localhost:8080')
-    .then((response) => {
-        return expect(response.status === 200)
+    axios.get(url).then((response) => {
+      expect(response.data.trim()).to.equal('Hello World');
     })
-  )
+  );
+
+  it('returns the correct status code', () =>
+    axios.get(url).then((response) => {
+      expect(response.status).to.equal(200);
+    })
+  );
 });
